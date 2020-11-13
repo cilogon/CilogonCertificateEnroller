b2a6f85b7c3f5de2d9b4792210fd2bdb75138d03
Scott Koranda <skoranda@gmail.com>
2020-11-13 07:56:59 -0600

    Update configured enrollment flow ID for PROD
    Update configured enrollment flow ID for PROD.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-Configure::write('CilogonCertificateEnroller.enrollment_flow_id', '233');
+Configure::write('CilogonCertificateEnroller.enrollment_flow_id', '235');
 Configure::write('CilogonCertificateEnroller.certificate_authenticator_description', 'CILogon Issued Certificate');
